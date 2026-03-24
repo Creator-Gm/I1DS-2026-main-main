19d9d5ce78ed6ac910b71ecef10482825d9dbf19
Creator-Gm <venom2guil.cm@gmail.com>
2026-03-24 10:15:44 -0300

    como funciona o incremento  e decremento

---
+++ aulas/12-lacoRepeticão.js
@@ -0,0 +1,6 @@
+//       laço de  repetiçao - for (para)
+//o professor te colocou de castigo e pediu para voce escrever  mil vezes a frese 
+// "eu vou prestar antençao e anotar tudo"
+for (let i = 0; i < 1000; i++) {//i++ == i = i+1
+
+}
