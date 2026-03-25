0348ae6ba445750a7c0f782db15050a463f913d2
Creator-Gm <venom2guil.cm@gmail.com>
2026-03-25 11:19:17 -0300

    ativ

--- atividades/09-listadeexercicios.js
+++ atividades/09-listadeexercicios.js
@@ -2,4 +2,39 @@ var nome = "Paulo";
 var idade = 16;
 var cidade = "ChiqueChiquebahia";
 console.log("Nome:", nome, "| idade?", idade, "| cidade?", cidade);
-let = 
+let numero1 =  4
+let numero2 = 5
+soma = numero1 + numero2
+console.log("a soma e", soma);
+let numeros1 =  10
+let numeros2 = 5
+sub = numeros1 - numeros2
+console.log("a subtração e", sub);
+let numer1 =  4
+let numer2 = 5
+mult = numer1 * numer2
+console.log("a multiplicação e", mult);
+let nume1 =  4
+let nume2 = 5
+diva = nume1 / nume2
+console.log("a divisão e", diva);
+console.log(5 % 4); 
+
+if (diva % 2 === 0) {
+  console.log("Número par");
+}
+
+function cont(base, altura) {
+    return base * altura;
+}
+
+let are = cont(10, 5);
+console.log("A área do retângulo é: " + are);
+
+
+let paral = 10*2*5
+
+console.log("volume do paralelepipedo e", paral);
+ let valor = 100 
+ desconto =10/100
+ valor-[valor*desconto]
