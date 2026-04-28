32646eecbe9a4a748c71ae2335b000ef28d2f226
Creator-Gm <venom2guil.cm@gmail.com>
2026-04-28 17:01:52 -0300

    atividade html

--- atividades(html)/atividades 2/js/scripts.js
+++ atividades(html)/atividades 2/js/scripts.js
@@ -1,17 +1,41 @@
+let n = 0
+
+const remove = (id) => {
+    let lista = document.getElementById("lista")
+    let elemento = document.getElementById(id)
+    lista.removeChild(elemento)
+
+    let contagem = lista.childElementCount
+    document.getElementById("contagem").innerText = contagem
+}
+
+const edit = (id) => {
+    
+}
+
 const cadastrar = (event) => {
-  // Parar a propagação padrão do evento
-  event.preventDefault();
-  // Capturar os valores do elementos por Id
-  let nome = document.getElementById("nome").value;
-  let email = document.getElementById("email").value;
-  // Capturando o elemento de lista por Id
-  let lista = document.getElementById("lista");
-  // Criar um novo elemento <li>Nome - Email</li>
-  let novoItem = document.createElement("li");
-  novoItem.innerText = nome + " - " + email;
-  // Adicionar o novo item na lista ja existente
-  lista.appendChild(novoItem);
-  // Limpar os campos
-  document.getElementById("nome").value = "";
-  document.getElementById("email").value = "";
-};
+    // parar a propagaão padrão do evento
+    event.preventDefault();
+
+
+    // capturando  os valores dos campos dos elementos por id
+    let nome = document.getElementById("nome").value;
+    let email = document.getElementById("email").value;
+
+    // capturando o elemento de lista por id
+    let lista = document.getElementById("lista");
+
+    // criar um novo elemento <li>Nome - Email</li>
+    let novoItem = document.createElement("li");
+    n++
+    let id = n
+    novoItem.innerHTML = id + " - " + nome + " - " + email + '<button onclick="remove('+ id +')" class="removeButton">Remover</button>' + '<button onclick="edit('+ id +')" class="editButton">Editar</button>';
+    novoItem.id = id
+
+    lista.appendChild(novoItem);
+    document.getElementById("nome").value = "";
+    document.getElementById("email").value = "";
+
+    let contagem = lista.childElementCount
+    document.getElementById("contagem").innerText = contagem
+}
