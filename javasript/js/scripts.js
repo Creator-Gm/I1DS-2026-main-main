e11613d8c36b7df4f9c20f986341bd2d93dbb1ed
Creator-Gm <venom2guil.cm@gmail.com>
2026-04-29 11:27:05 -0300

    calculadora

--- javasript/js/scripts.js
+++ javasript/js/scripts.js
@@ -14,3 +14,37 @@ const zerar= () => {
   vaatual = 0;
   contador.innerHTML = vaatual;
 };
+//exercicio 02
+const mudarcor = () => {
+    //capturar a cor selecionada pelo usuario
+let cor = document.getElementById("colorpi").value;
+
+//buscar o elemento alvo 'quadro
+let quadro = document.getElementById("quadro");
+//aplicar cor
+quadro.style.background = cor
+}
+/* EXERCICIO 03 */
+let num1 = document.getElementById("num1");
+let num2 = document.getElementById("num2");
+let resultado = document.getElementById("resultado");
+
+const somar = () => {
+  let valor = parseFloat(num1.value) + parseFloat(num2.value);
+  resultado.innerText = valor;
+};
+
+const subtrair = () => {
+  let valor = parseFloat(num1.value) - parseFloat(num2.value);
+  resultado.innerText = valor;
+};
+
+const multiplicar = () => {
+  let valor = parseFloat(num1.value) * parseFloat(num2.value);
+  resultado.innerText = valor;
+};
+
+const dividir = () => {
+  let valor = parseFloat(num1.value) / parseFloat(num2.value);
+  resultado.innerText = valor;
+};
