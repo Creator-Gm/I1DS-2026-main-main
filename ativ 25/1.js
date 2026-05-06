dc53754d100c29a3a1e1f2c267b253440853a61b
Creator-Gm <venom2guil.cm@gmail.com>
2026-05-06 16:18:00 -0300

    25 atividades corrigidas

---
+++ ativ 25/1.js
@@ -0,0 +1,16 @@
+var x = 10;
+while (x > 10) {
+  console.log("entrei no laço while...");
+  x = 0;
+}
+console.log("terminei");
+
+//somar enquanto while menor que 10
+var soma = 0;
+var num = 0;
+while(num < 10){
+soma += num;
+num++
+console.log("somando.....",soma);
+console.log("numero....",num);
+}
